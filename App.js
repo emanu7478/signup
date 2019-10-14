c1eae84a4ed6139592432a78094037b6d3f70469
emanu7478 <emanu7478@gmail.com>
2019-10-14 12:08:54 +0530

    first

---
+++ App.js
@@ -0,0 +1,223 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Image,
+  Picker,
+  Alert,
+} from 'react-native';
+import {CheckBox} from 'react-native-elements';
+
+const checks = ['Sing', 'dance', 'draw'];
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      Checked: [],
+    };
+  }
+
+
+  update(text, field) {
+    if (field == 'name') {
+      this.setState({
+        name: text,
+      });
+    } else if (field == 'email') {
+      this.setState({
+        email: text,
+      });
+    }
+  }
+
+  submit() {
+    let abc = {};
+    abc.name = this.state.name;
+    abc.email = this.state.email;
+    console.log(abc)
+  }
+
+// checkItem = (item) => {
+//   const { checked } = this.state;
+//   let newArr = [];
+//
+//   if (!checked.includes(item)) {
+//     newArr = [...checked, item];
+//   } else {
+//     newArr = checked.filter(a => a !== item);
+//   }
+//   this.setState({ checked: newArr }, () => console.log('updated state', newArr))
+// };
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.inputs}
+                placeholder="Full name"
+                underlineColorAndroid="transparent"
+                onChangeText={text => this.update(text, 'name')}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+                style={styles.inputs}
+                placeholder="Email"
+                keyboardType="email-address"
+                underlineColorAndroid="transparent"
+                onChangeText={text => this.update(text, 'email')}
+            />
+          </View>
+
+          <View>
+            {checks.map((item,i) => {
+              return (
+                  <CheckBox
+                      key={i}
+                      title={item}
+                      checked={true}
+                      onPress={() => this.toggleCheckbox()}
+                  />
+              );
+            })}
+          </View>
+
+          <View style={{flexDirection: 'column'}}>
+            <Picker
+                selectedValue={this.state.language}
+                style={{height: 50, width: 100}}
+                onValueChange={(itemValue, itemIndex) =>
+                    this.setState({language: itemValue})
+                }>
+              <Picker.Item label="kerala" value="kerala"/>
+              <Picker.Item label="karnataka" value="karnataka"/>
+            </Picker>
+          </View>
+          <TouchableOpacity
+              style={styles.btnByRegister}
+              onPress={() => this.submit()}
+          />
+        </View>
+    );
+  }
+}
+
+const resizeMode = 'center';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DCDCDC',
+  },
+  inputContainer: {
+    borderBottomColor: '#F5FCFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 300,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    shadowColor: '#808080',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: '#FFFFFF',
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginRight: 15,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 300,
+    borderRadius: 30,
+    backgroundColor: 'transparent',
+  },
+  btnByRegister: {
+    height: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+    width: 300,
+    backgroundColor: 'blue',
+  },
+  loginButton: {
+    backgroundColor: '#00b5ec',
+
+    shadowColor: '#808080',
+    shadowOffset: {
+      width: 0,
+      height: 9,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+
+    elevation: 19,
+  },
+  loginText: {
+    color: 'white',
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+  },
+  textByRegister: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+  },
+});
+export default App;
